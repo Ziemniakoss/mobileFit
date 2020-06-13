91e8eddf1b84930ef805f1d7311576c4d03e63a9
ZIemniakoss <rawa.przemek@gmail.com>
2020-06-13 16:13:54 +0200

    wagi

--- app/src/main/java/com/mobilefit/data/weights/WeightMeasurementsDataSource.kt
+++ app/src/main/java/com/mobilefit/data/weights/WeightMeasurementsDataSource.kt
@@ -1,7 +1,17 @@
 package com.mobilefit.data.weights
 
+import com.google.gson.Gson
+import com.mobilefit.Variables
 import com.mobilefit.data.Result
 import com.mobilefit.data.exceptions.NotFoundException
+import com.mobilefit.data.users.UserDataSource
+import com.mobilefit.data.users.UserRepository
+import com.mobilefit.dto.WeightCreationDTO
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
+import java.io.IOException
 
 class WeightMeasurementsDataSource {
 	private val measurements = ArrayList<WeightMeasurement>()
@@ -13,15 +23,40 @@ class WeightMeasurementsDataSource {
 	}
 
 	fun add(measurement: WeightMeasurement) {
-
+		val t = Thread {
+			val dto = WeightCreationDTO(measurement.value)
+			val req = Request.Builder()
+				.url("${Variables.urlToServer}/weights")
+				.post(RequestBody.create(Variables.jsonMediaType, Gson().toJson(dto)))
+				.addHeader("Authorization", "Bearer ${UserRepository.user?.jwt}")
+				.build()
+			OkHttpClient().newCall(req).execute().use {
+				val response: Response = it
+			}
+		}
+		t.start()
 	}
 
 	fun getAll(): Result<List<WeightMeasurement>> {
-		return Result.Success(measurements as List<WeightMeasurement>)
-
+		var result :Result<List<WeightMeasurement>> = Result.Error(IOException())
+		val t = Thread {
+			val req = Request.Builder()
+				.url("${Variables.urlToServer}/weights")
+				.header("Authorization", "Bearer ${UserRepository.user?.jwt}")
+				.get()
+				.build()
+			OkHttpClient().newCall(req).execute().use {
+				val response:Response = it
+				val weights = Gson().fromJson(response.body()?.string(), mutableListOf<WeightMeasurement>()::class.java)
+				result = Result.Success(weights)
+			}
+		}
+		t.start()
+		t.join()
+		return result
 	}
 
-	fun getLast(): Result<WeightMeasurement> {
+	fun getLast(): Result<WeightMeasurement> {//todo
 		if (measurements.size < 1)
 			return Result.Error(NotFoundException())
 		var res = measurements[0]
--- app/src/main/java/com/mobilefit/ui/login/LoginViewModelFactory.kt
+++ app/src/main/java/com/mobilefit/ui/login/LoginViewModelFactory.kt
@@ -15,7 +15,7 @@ class LoginViewModelFactory : ViewModelProvider.Factory {
 	override fun <T : ViewModel> create(modelClass: Class<T>): T {
 		if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
 			return LoginViewModel(
-				UserRepository(dataSource = UserDataSource)
+				UserRepository
 			) as T
 		}
 		throw IllegalArgumentException("Unknown ViewModel class")
