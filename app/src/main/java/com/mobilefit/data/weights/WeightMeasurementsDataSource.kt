bc0ba62c3e093628259f3885b3739d487f439965
ZIemniakoss <rawa.przemek@gmail.com>
2020-06-13 18:35:06 +0200

    wagi - wstęp

--- app/src/main/java/com/mobilefit/data/weights/WeightMeasurementsDataSource.kt
+++ app/src/main/java/com/mobilefit/data/weights/WeightMeasurementsDataSource.kt
@@ -1,9 +1,11 @@
 package com.mobilefit.data.weights
 
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.mobilefit.Variables
 import com.mobilefit.data.Result
 import com.mobilefit.data.exceptions.NotFoundException
+import com.mobilefit.data.exercises.ExerciseCategory
 import com.mobilefit.data.users.UserDataSource
 import com.mobilefit.data.users.UserRepository
 import com.mobilefit.dto.WeightCreationDTO
@@ -12,14 +14,28 @@ import okhttp3.Request
 import okhttp3.RequestBody
 import okhttp3.Response
 import java.io.IOException
+import java.lang.reflect.Type
 
-object  WeightMeasurementsDataSource {
+object WeightMeasurementsDataSource {
 	private val measurements = ArrayList<WeightMeasurement>()
 	fun get(id: Int): Result<WeightMeasurement> {
-		for (i in 0 until measurements.size)
-			if (measurements[i].id == id)
-				return Result.Success(measurements[i])
-		return Result.Error(NotFoundException())
+		var result:Result<WeightMeasurement> = Result.Error(IOException())
+		val t = Thread{
+			val req = Request.Builder()
+				.get()
+				.url("${Variables.urlToServer}/weights/${id}")
+				.addHeader("Authorization", "Bearer ${UserRepository.user?.jwt}")
+				.build()
+			OkHttpClient().newCall(req).execute().use {
+				if(!it.isSuccessful){
+					result = Result.Error(NotFoundException())//niedokładnie ale przejdzie
+				}else{
+					val fooType: Type? = object : TypeToken<WeightMeasurement?>() {}.type
+					result = Result.Success(Gson().fromJson(it.body()?.string(), fooType))
+				}
+			}
+		}
+		return result
 	}
 
 	fun add(measurement: WeightMeasurement) {
@@ -35,10 +51,11 @@ object  WeightMeasurementsDataSource {
 			}
 		}
 		t.start()
+		t.join()
 	}
 
 	fun getAll(): Result<List<WeightMeasurement>> {
-		var result :Result<List<WeightMeasurement>> = Result.Error(IOException())
+		var result: Result<List<WeightMeasurement>> = Result.Error(IOException())
 		val t = Thread {
 			val req = Request.Builder()
 				.url("${Variables.urlToServer}/weights")
@@ -46,8 +63,9 @@ object  WeightMeasurementsDataSource {
 				.get()
 				.build()
 			OkHttpClient().newCall(req).execute().use {
-				val response:Response = it
-				val weights = Gson().fromJson(response.body()?.string(), mutableListOf<WeightMeasurement>()::class.java)
+				val response: Response = it
+				val fooType: Type? = object : TypeToken<List<WeightMeasurement?>?>() {}.type
+				val weights = Gson().fromJson<List<WeightMeasurement>>(response.body()?.string(), fooType)
 				result = Result.Success(weights)
 			}
 		}
