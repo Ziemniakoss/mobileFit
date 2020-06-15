0508e638de9adc887faa4c1827bdfab6eeb52592
krysztok <krzysztof.krysztofik@wp.pl>
2020-06-15 02:33:32 +0200

    wyswietlanie expa i levela

--- app/src/main/java/com/mobilefit/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mobilefit/ui/home/HomeFragment.kt
@@ -10,6 +10,8 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment
 import com.mobilefit.R
+import com.mobilefit.data.quests.QuestRepository
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -23,21 +25,33 @@ class HomeFragment : Fragment() {
 		homeViewModel =
 			ViewModelProviders.of(this).get(HomeViewModel::class.java)
 		val root = inflater.inflate(R.layout.fragment_home, container, false)
-		val textView: TextView = root.findViewById(R.id.text_home)
-		homeViewModel.text.observe(viewLifecycleOwner, Observer {
-			textView.text = it
-		})
+
 		return root
 	}
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 
-		view.findViewById<View>(R.id.button_home).setOnClickListener {
-			val action = HomeFragmentDirections
-				.actionHomeFragmentToHomeSecondFragment("From HomeFragment")
-			NavHostFragment.findNavController(this@HomeFragment)
-				.navigate(action)
+		var questList = QuestRepository().getAll().get()
+
+		var exp = 0
+
+		for(quest in questList!!){
+			if(quest.completed == true){
+				exp += quest.xp
+			}
 		}
+
+		var level = exp / 2000
+		exp = exp % 2000
+
+
+		var expToNextLevel = 2000
+		var procent = (exp.toFloat()/expToNextLevel.toFloat() *100).toInt()
+		progressBar.progress = procent
+		level2_view.text = level.toString()
+		exp2_textview.text = exp.toString()
+		exptonextlevel2_textview.text = expToNextLevel.toString()
+
 	}
 }
